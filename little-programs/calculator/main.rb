712cab5ed67c910e1a5a86b060fa1da78d24d1f7
Solindek Developer <83218819+SolindekDev@users.noreply.github.com>
2021-11-17 12:46:55 +0100

    em yeee.... i deleted ( and )

--- little-programs/calculator/main.rb
+++ little-programs/calculator/main.rb
@@ -1,30 +1,30 @@
-puts ("Give first number")
+puts "Give first number"
 first_num = gets().chomp.to_i
-puts ("Give second number")
+puts "Give second number"
 second_num = gets().chomp.to_i
-puts ("What do you want to do with this two numbers?\ndivide, add, multipe, subtract or all?")
+puts "What do you want to do with this two numbers?\ndivide, add, multipe, subtract or all?"
 type = gets().chomp.downcase()
 
 if type == "add" || type == "multipe" || type == "subtract" || type == "divide" || type == "all"
     if type == "add"
-        puts ("Add: #{first_num + second_num}")
+        puts "Add: #{first_num + second_num}"
     end
     if type == "multipe"
-        puts ("Multipe: #{first_num * second_num}")
+        puts "Multipe: #{first_num * second_num}"
     end
     if type == "subtract"
-        puts ("Subtract: #{first_num - second_num}")
+        puts "Subtract: #{first_num - second_num}"
     end
     if type == "divide"
         puts ("Divide: #{first_num / second_num}")
     end
     if type == "all"
-        puts ("Add: #{first_num + second_num}")
-        puts ("Multipe: #{first_num * second_num}")
-        puts ("Subtract: #{first_num - second_num}")
-        puts ("Divide: #{first_num / second_num}")
+        puts "Add: #{first_num + second_num}"
+        puts "Multipe: #{first_num * second_num}"
+        puts "Subtract: #{first_num - second_num}"
+        puts "Divide: #{first_num / second_num}"
     end
 else
-    puts ("Unknown type to do")
+    puts "Unknown type to do"
 end
 

8414873cd17882966860133e19b45081b1174285
Solindek Developer <83218819+SolindekDev@users.noreply.github.com>
2021-11-17 12:28:50 +0100

    Create math.rb

---
+++ math/math.rb
@@ -0,0 +1,29 @@
+# Math file because i'm smart guy 🦉
+
+# Adding
+example_variable = 1 + 2 # 3
+puts example_variable
+
+# Subtract
+example_variable = 1 - 2 # -1
+puts example_variable
+
+# Mul
+example_variable = 2 * 2 # 4
+puts example_variable
+
+# Divide
+example_variable = 2 / 2 # 1
+puts example_variable
+
+# Power
+example_variable = 2**3 # 8
+puts example_variable
+
+# Numbers to string
+num = 10
+puts("My favorite number is " + num.to_s())
+
+# or
+
+puts "My favorite number is #{num}"

bd8d546cb8cd330d8dc633db8abbb325e8b236af
Solindek Developer <83218819+SolindekDev@users.noreply.github.com>
2021-11-17 12:07:49 +0100

    Update strings.rb

---
+++ combinations-with-strings/strings.rb
@@ -0,0 +1,19 @@
+# Upcase
+example_variable = "example"
+puts example_variable.upcase() # EXAMPLE
+
+# Downcase
+example_variable1 = "EXAMPLE"
+puts example_variable1.downcase() # example
+
+# Strip
+example_variable2 = "        foo         " 
+puts example_variable2.strip() # foo
+
+# Length of string
+example_variable3 = "dsadsadads"
+puts example_variable3.length() # 10
+
+# Include
+example_variable3 = "dsadsadads"
+puts example_variable3.include?("dd") # false 

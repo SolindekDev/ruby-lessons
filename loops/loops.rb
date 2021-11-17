ca27104d082d230f7b167471e44f889c48f24def
Solindek Developer <83218819+SolindekDev@users.noreply.github.com>
2021-11-17 13:37:16 +0100

    Loops

---
+++ loops/loops.rb
@@ -0,0 +1,29 @@
+# While 
+
+index = 1
+
+while index <= 5
+    puts index 
+    index += 1
+end
+
+# For
+
+for b in 0...2 do
+    puts "#{b}"
+end
+
+# For array
+
+# Kc <3
+friends = ["KaYn", "Kłapouch", "Nes0x"]
+
+for friend in friends
+    puts friend
+end
+
+# Each
+
+friends.each do |friend|
+    puts friend
+end

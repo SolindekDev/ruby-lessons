54d054f84a5e6420c01cfedee0cf7fd52ed93edc
Solindek Developer <83218819+SolindekDev@users.noreply.github.com>
2021-11-17 11:55:51 +0100

    .

--- variables/variables.rb
+++ variables/variables.rb
@@ -3,7 +3,7 @@ languages = ["C#", "Java", "JavaScript", "Ruby", "GoLang"]
 gender = true
 age = 2137
 
-puts "My name is #{name}"
-puts "My age is #{age}"
-puts "My gender is #{gender} (true = men, false = female)"
-puts "My favorites languages are #{languages.join(", ")}"
+puts("My name is #{name}")
+puts("My age is #{age}")
+puts("My gender is #{gender} (true = men, false = female)")
+puts("My favorites languages are #{languages.join(", ")}")

bae872d7ac78b8f4b490d2baf8b6c39221a31799
Solindek Developer <83218819+SolindekDev@users.noreply.github.com>
2021-11-17 11:45:07 +0100

    Update variables.rb

--- variables/variables.rb
+++ variables/variables.rb
@@ -1,5 +1,9 @@
 name = "Bruno"
 languages = ["C#", "Java", "JavaScript", "Ruby", "GoLang"]
+gender = true
+age = 2137
 
 puts "My name is #{name}"
-puts "My name is #{languages.join(" ")}"
+puts "My age is #{age}"
+puts "My gender is #{gender} (true = men, false = female)"
+puts "My favorites languages are #{languages.join(", ")}"

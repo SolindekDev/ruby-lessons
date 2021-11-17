d948c968b294b1ae0a4871f1b25149f335c81af7
Solindek Developer <83218819+SolindekDev@users.noreply.github.com>
2021-11-17 12:20:26 +0100

    Moreeee

--- combinations-with-strings/strings.rb
+++ combinations-with-strings/strings.rb
@@ -14,6 +14,11 @@ puts example_variable2.strip() # foo
 example_variable3 = "dsadsadads"
 puts example_variable3.length() # 10
 
+# or
+
+puts example_variable3.size() # 10
+
+
 # Include
 example_variable4 = "dsadsadads"
 puts example_variable3.include?("dd") # false 
@@ -32,3 +37,46 @@ puts example_variable4[0,3] # fda
 # Where is in index a character
 example_variable5 = "dasdasdada"
 puts example_variable5.index("a") # 1
+
+# Is string empty
+example_variable6 = ""
+puts example_variable5.empty?() # true
+
+# Add numbers in puts
+example_variable7 = 12
+example_variable8 = 12
+puts "Add: #{example_variable7 + example_variable8}" # 24
+
+# Starts with
+example_variable9 = "Hello, world!"
+puts example_variable9.start_with?("Hello") # true
+
+# End with
+example_variable10 = "Hello, world!"
+puts example_variable9.end_with?("world!") # true
+
+# Delete suffix
+example_variable11 = "Hello mum!"
+example_variable11 = example_variable11.delete_suffix("mum!")
+puts example_variable11
+
+# Delete prefix
+example_variable11 = "Hello mum!"
+example_variable11 = example_variable11.delete_prefix("Hello ")
+puts example_variable11
+
+# Convert string into an integer
+example_variable12 = "12"
+example_variable12 = example_variable12.to_i # 12
+puts example_variable12
+
+# Append characters
+example_variable13 = ""
+
+example_variable13 << "Hi"
+example_variable13 << " "
+example_variable13 << "i'am"
+example_variable13 << " "
+example_variable13 << "Bruno"
+
+puts example_variable13
